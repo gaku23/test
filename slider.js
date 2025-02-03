0e59e928e9c42dc92cd5f84c1313bb1f33d643fa
gaku23 <gakuch4@gmail.com>
2025-02-03 23:46:08 +0700

    slider new

--- slider.js
+++ slider.js
@@ -28,19 +28,3 @@ const swiperSlider = new Swiper('.swiper', {
       disableOnInteraction: false, 
     },
   });
-
-function checkExam(array1, array2) {
-  let count = 0;
-   for (let i = 0; i < array1.length;i++){
-     if (array1[i] === array2[i]){
-       count += 4
-     }
-     else if (array1[i] != array2[i]){
-       count -= 1
-     }
-  }
-   if(count < 0) return 0
-   else return count
- }
-
- console.log(checkExam(["a", "a", "b", "b"], ["a", "c", "b", "d"]))
